23de1660142492684e1ff4f0e739947742689368
Kranus37 <samuel.resende6637@gmail.com>
2025-11-14 14:38:36 -0300

    Update ItemVenda.java

--- src/ItemVenda.java
+++ src/ItemVenda.java
@@ -1,24 +1,15 @@
-/**
- * Classe que representa um item em uma venda.
- */
+//Classe que representa um item em uma venda
 public class ItemVenda {
     private Produto produto;
     private int quantidade;
 
-    /**
-     * Construtor.
-     * @param produto O produto vendido.
-     * @param quantidade A quantidade do produto.
-     */
+    //Construtor. @param produto O produto vendido. @param quantidade A quantidade do produto
     public ItemVenda(Produto produto, int quantidade) {
         this.produto = produto;
         this.quantidade = quantidade;
     }
 
-    /**
-     * Calcula o subtotal do item.
-     * @return O valor total deste item (preço * quantidade).
-     */
+    //Calcula o subtotal do item. @return O valor total deste item (preço * quantidade)
     public double getSubtotal() {
         return produto.getPreco() * quantidade;
     }
